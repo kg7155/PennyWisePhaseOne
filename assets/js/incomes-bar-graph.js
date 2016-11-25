a5e72fe2715bf747b5b1f4158e3c9092149e6da0
Karmen <karmen.gostisa@hotmail.com>
2016-11-25 19:39:17 +0100

    Added bar graph in incomes.html

--- assets/js/incomes-bar-graph.js
+++ assets/js/incomes-bar-graph.js
@@ -3,10 +3,10 @@ canvas.width = 800;
 canvas.height = 400;
 var context = canvas.getContext("2d");
 
-var data = [0, 0, 6.2, 33.4, 0, 3.4, 6.2, 9, 120.0, 65.2, 12.5, 0, 2.5, 2.3, 4.5, 24.4, 5, 2, 65, 2, 4, 5, 35.2, 41, 23.4, 44, 0, 2, 1, 3, 50];
+var data = [1250, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var maxValue = Math.max.apply(Math, data);
 var minValue = Math.min.apply(Math, data);
-var step = 20;
+var step = 250;
 var cols = 31;
 var rows = (maxValue - minValue) / step + 1;
 var margin = 10;
@@ -44,6 +44,8 @@ context.beginPath();
 // draw bars
 var c;
 for (i = 1; i <= cols; i++) {
+    if (data[i-1] == 0)
+        continue;
     c = 0;
     for (j = maxValue; j >= minValue; j -= step) {
         if (data[i-1] < j)

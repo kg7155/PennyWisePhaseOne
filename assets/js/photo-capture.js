d64738cf01dc52ef260128d80bcd2e9a6a08e9f9
Karmen <karmen.gostisa@hotmail.com>
2016-11-26 16:24:58 +0100

    Debugging photo capture

--- assets/js/photo-capture.js
+++ assets/js/photo-capture.js
@@ -1,15 +1,25 @@
-var video = document.querySelector("#video");
+var video = document.querySelector('#video');
+var constraints = { audio: true, video: { width: 150, height: 200 } }; 
 
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+navigator.mediaDevices.getUserMedia(constraints)
+.then(function(mediaStream) {
+  var video = document.querySelector('video');
+  video.srcObject = mediaStream;
+  video.onloadedmetadata = function(e) {
+    video.play();
+  };
+})
+.catch(function(err) { console.log(err.name + ": " + err.message); });
 
-if (navigator.getUserMedia) {       
-    navigator.getUserMedia({video: true}, handleVideo, videoError);
-}
+
+// if (!!navigator.getUserMedia) {       
+//     navigator.getUserMedia({video: true}, handleVideo, videoError);
+// }
  
-function handleVideo(stream) {
-    video.src = window.URL.createObjectURL(stream);
-}
+// function handleVideo(stream) {
+//     video.src = window.URL.createObjectURL(stream);
+// }
  
-function videoError(e) {
-    alert("Error!" + e);
-}
+// function videoError(e) {
+//     alert("Error!" + e);
+// }
